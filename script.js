da2263cc64f3aadd34e7e2e933ed1cb27998b1b7
DylanM01 <dylanmprogramming@gmail.com>
2023-11-30 18:53:49 -0500

    Build algorithm for factoring numbers

---
+++ script.js
@@ -0,0 +1,17 @@
+let numToFactor = prompt('give a number to factor')
+let number = parseFloat(numToFactor);
+const container = document.getElementById("container");
+
+function factor(num) {
+    let factors = [];
+
+    for (let i = 1; i <= num; i++) {
+        if (num % i === 0) {
+            factors.push(i);
+        }
+    }
+
+    container.innerText = factors;
+}
+
+factor(number);
