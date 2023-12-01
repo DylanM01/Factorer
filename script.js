3dafc1194cc405cc70bbef07cf5cfb127f0f3625
DylanM01 <dylanmprogramming@gmail.com>
2023-11-30 21:20:55 -0500

    Added CSS

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-let numToFactor = prompt('give a number to factor')
-let number = parseFloat(numToFactor);
 const container = document.getElementById("container");
+const factorButton = document.getElementById('factor');
 
 function factor(num) {
     let factors = [];
@@ -11,7 +10,16 @@ function factor(num) {
         }
     }
 
-    container.innerText = factors; // this is my project specific
+    // Join the factors array into a string with commas
+    let factorsString = factors.join(', ');
+
+    // Append the factorsString followed by a <br> tag to the existing content of the container
+    container.innerHTML += factorsString + '<br>';
 }
 
-factor(number);
+factorButton.addEventListener('click', () => {
+    let numToFactor = prompt('give a number to factor');
+    let number = parseInt(numToFactor);
+
+    factor(number);
+});
